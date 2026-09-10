e0b3ade7a818b19899a555a33d0151b2e32a83a1
IOS-Ed <isabel.osullivan@education.vic.gov.au>
2026-09-10 11:06:12 +1000

    Update community services tile

--- custom-catalog.js
+++ custom-catalog.js
@@ -278,7 +278,7 @@ var defineTiles = async function () {
    ["Education Workforces", "/?category%5Bid%5D=64839&category%5Bname%5D=deworkforces", githubpage + "assets/img/de-wf-tile.png"],
    ["Families Fairness and Housing", "/?category%5Bid%5D=64841&category%5Bname%5D=dffhworkforces", githubpage + "assets/img/dffh-wf-tile.png"],
    ["Health Workforces", "/?category%5Bid%5D=64842&category%5Bname%5D=dhworkforces", githubpage + "assets/img/dh-wf-tile.png"],
-   ["Community Service Workforces", "/?category%5Bid%5D=64840&category%5Bname%5D=csworkforces", githubpage + "assets/img/IS-DFFH-elearn-tile-resize.png"],
+   ["Community Services Workforces", "/?category%5Bid%5D=64840&category%5Bname%5D=csworkforces", githubpage + "assets/img/IS-DFFH-elearn-tile-resize.png"],
    ["Child Link", "/?category%5Bid%5D=64838&category%5Bname%5D=childlink", githubpage + "assets/img/childlink-logo.png"],
    ["Justice Training", "/?category%5Bid%5D=64843&category%5Bname%5D=djcsworkforces", githubpage + "assets/img/scales-3.png"]
   ];
